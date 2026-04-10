c3ca3a5d0cca6b7f3cb9d1708062f87700a8ca8c
homatse <m.hoenisch@schrack-seconet.com>
2026-04-10 13:55:14 +0000

    Add dynamic API URL base configuration based on CODESPACE_NAME environment variable

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+
+# Build API URL base with CODESPACE_NAME environment variable
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+if CODESPACE_NAME:
+    API_URL_BASE = f"https://{CODESPACE_NAME}-8000.app.github.dev"
+else:
+    API_URL_BASE = "http://localhost:8000"
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
